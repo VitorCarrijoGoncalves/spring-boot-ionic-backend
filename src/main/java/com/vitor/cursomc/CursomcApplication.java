934392461f6e5ba46d9a6fa570943356840c8e5c
System Administrator <root@MacBookPros-MacBook-Pro.local>
2018-12-17 15:34:01 -0200

    Alterações feitas até a aula de número 24, minuto 11

--- src/main/java/com/vitor/cursomc/CursomcApplication.java
+++ src/main/java/com/vitor/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.vitor.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,12 @@ import com.vitor.cursomc.domain.Cidade;
 import com.vitor.cursomc.domain.Cliente;
 import com.vitor.cursomc.domain.Endereco;
 import com.vitor.cursomc.domain.Estado;
+import com.vitor.cursomc.domain.Pagamento;
+import com.vitor.cursomc.domain.PagamentoComBoleto;
+import com.vitor.cursomc.domain.PagamentoComCartao;
+import com.vitor.cursomc.domain.Pedido;
 import com.vitor.cursomc.domain.Produto;
+import com.vitor.cursomc.domain.enums.EstadoPagamento;
 import com.vitor.cursomc.domain.enums.TipoCliente;
 import com.vitor.cursomc.repositories.CategoriaRepository;
 import com.vitor.cursomc.repositories.CidadeRepository;
@@ -91,6 +97,19 @@ public class CursomcApplication implements CommandLineRunner {
 		 clienteRepository.saveAll( Arrays.asList( cli1 ) );
 		 
 		 enderecoRepository.saveAll( Arrays.asList( e1, e2 ) );
+		 
+		 SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		 
+		 Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		 Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		 
+		 Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		 ped1.setPagamento(pagto1);
+		 
+		 Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null );
+		ped2.setPagamento(pagto2);
 		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		 
 	}
 }
--- src/main/java/com/vitor/cursomc/domain/Cliente.java
+++ src/main/java/com/vitor/cursomc/domain/Cliente.java
@@ -34,6 +34,9 @@ public class Cliente implements Serializable {
 	@OneToMany( mappedBy="cliente" )
 	private List<Endereco> enderecos = new ArrayList<>(  );
 	
+	@OneToMany( mappedBy="cliente" )
+	private List<Pedido> pedidos = new ArrayList<>( );  
+	
 	@ElementCollection
 	@CollectionTable( name="TELEFONE" )
 	private Set<String> telefones = new HashSet<>(  ); // set é uma coleção, que não aceita repetições
@@ -104,6 +107,14 @@ public class Cliente implements Serializable {
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	} 
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
 	
 	@Override
 	public int hashCode() {
