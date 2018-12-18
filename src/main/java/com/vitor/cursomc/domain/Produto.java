1552989bbbcb28297d513ba1822e3f438578601f
System Administrator <root@MacBookPros-MacBook-Pro.local>
2018-12-18 13:36:34 -0200

    Inclusão da aula 25

--- src/main/java/com/vitor/cursomc/domain/Produto.java
+++ src/main/java/com/vitor/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.vitor.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -35,6 +37,8 @@ public class Produto implements Serializable {
 		inverseJoinColumns = @JoinColumn(name = "categoria_id")) //  
 		private List<Categoria> categorias = new ArrayList<>();
 		
+		private Set<ItemPedido> itens = new HashSet<>();
+		
 		public Produto() {}
 
 		public Produto(Integer id, String nome, Double preco) {
@@ -42,6 +46,14 @@ public class Produto implements Serializable {
 			this.id = id;
 			this.nome = nome;
 			this.preco = preco;
+		}
+		
+		public List<Pedido> getPedidos() {
+			List<Pedido> lista = new ArrayList<>();
+			for(ItemPedido x : itens) {
+				lista.add(x.getPedido());
+			}
+			return lista;
 		}
 
 		public Integer getId() {
@@ -99,6 +111,14 @@ public class Produto implements Serializable {
 			} else if (!id.equals(other.id))
 				return false;
 			return true;
+		}
+
+		public Set<ItemPedido> getItens() {
+			return itens;
+		}
+
+		public void setItens(Set<ItemPedido> itens) {
+			this.itens = itens;
 		}
 		
 }
