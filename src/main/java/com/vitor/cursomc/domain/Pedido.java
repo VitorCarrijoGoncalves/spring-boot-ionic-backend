d3751edb848c7e62d006ddd7e71c651247de6b1a
System Administrator <root@MacBookPros-MacBook-Pro.local>
2018-12-21 13:27:25 -0200

    Atualização utilizando somente JsonIgnore

--- src/main/java/com/vitor/cursomc/domain/Pedido.java
+++ src/main/java/com/vitor/cursomc/domain/Pedido.java
@@ -16,7 +16,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Pedido implements Serializable {
@@ -30,11 +29,9 @@ public class Pedido implements Serializable {
 	@JsonFormat(pattern="dd/mm/yyyy HH:mm") // formatar a data no json
 	private Date instante;
 	
-	@JsonManagedReference
 	@OneToOne( cascade=CascadeType.ALL, mappedBy="pedido" )
 	private Pagamento pagamento;
 	
-	@JsonManagedReference // aquivo json de cliente, referencia pedido
 	@ManyToOne
 	@JoinColumn( name="cliente_id" )
 	private Cliente cliente;
--- src/main/java/com/vitor/cursomc/domain/Produto.java
+++ src/main/java/com/vitor/cursomc/domain/Produto.java
@@ -15,7 +15,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
@@ -32,7 +31,7 @@ public class Produto implements Serializable {
 		// relação muito para muitos = cria uma tabela no meio desta relação, aonde esta tabela recebe a chave estrangeira de produto
 		// e categoria
 		
-		@JsonBackReference
+		@JsonIgnore
 		@ManyToMany
 		@JoinTable(name = "PRODUTO_CATEGORIA", 
 		joinColumns = @JoinColumn(name = "produto_id"), 
