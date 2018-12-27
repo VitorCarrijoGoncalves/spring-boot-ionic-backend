d936bd15c1feb69171ea7c1064305ed9e1900cef
System Administrator <root@MacBookPros-MacBook-Pro.local>
2018-12-27 18:02:40 -0200

    Atualizando Categoria co PUT, outros pequenos ajustes

--- src/main/java/com/vitor/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/vitor/cursomc/resources/CategoriaResource.java
@@ -22,8 +22,8 @@ public class CategoriaResource {
 	private CategoriaService service;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Categoria obj = service.buscar(id);
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
+		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
 	
@@ -40,6 +40,14 @@ public class CategoriaResource {
 				path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 		// ResponseEntity.created = constroi essa uri para min, o build gera a resposta no brownser
+	}
+	
+	//Usando a requisição PUT para fazer alteração de dados 
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+		obj.setId(id); // setando o id que sera alterado neste metodo
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build(); // noContent() = response com conteudo vazio
 	}
 
 }
--- src/main/java/com/vitor/cursomc/services/CategoriaService.java
+++ src/main/java/com/vitor/cursomc/services/CategoriaService.java
@@ -15,7 +15,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		if (obj == null) {
 			throw new ObjectNotFoundException("Objeto não encontrado! Id = " + id + ", Tipo: " + Categoria.class.getName());
@@ -26,6 +26,11 @@ public class CategoriaService {
 	public Categoria insert(Categoria obj) {
 		obj.setId(null); // o Objeto novo a ser inserido precisa o ter p id nulo
 		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj); // id nulo, objeto inserido, id diferente de nulo, objeto a ser atualizado
 	}
 
 }
