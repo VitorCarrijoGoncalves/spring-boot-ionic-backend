91dd21b46ed265f050d1997b59925d8499230238
System Administrator <root@MacBookPros-MacBook-Pro.local>
2019-01-03 13:25:26 -0200

    Listando todas as categorias com DTO

--- src/main/java/com/vitor/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/vitor/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.vitor.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.vitor.cursomc.domain.Categoria;
+import com.vitor.cursomc.dto.CategoriaDTO;
 import com.vitor.cursomc.services.CategoriaService;
 
 @RestController
@@ -54,6 +57,16 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		// stream = percorre a lista
+		// faz uma operação pra cada elemento da lista
+		// .collect(Collector.toList()) = eu consigo converter uma lista para outra lista
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
